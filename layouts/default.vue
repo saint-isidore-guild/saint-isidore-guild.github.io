9168aa9de8fc92a684f748816e535001ab07c9e4
Mike Truso <mike@agileorbit.com>
2021-12-30 16:38:02 -0600

    remove hamburger menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,26 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
       fixed
       app
+      flat
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <v-btn
+        v-for="link in items"
+        :key="link.to"
+        :to="link.to"
+        text
+      >
+        {{ link.title }}
+      </v-btn>
     </v-app-bar>
     <v-main>
       <nuxt />
     </v-main>
     <v-footer
-      :absolute="!fixed"
+      :absolute="false"
       app
     >
       <span>&copy; 2017 - {{ new Date().getFullYear() }}</span>
@@ -49,13 +32,10 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
@@ -64,7 +44,6 @@ export default {
           to: '/about'
         }
       ],
-      miniVariant: false,
       title: 'Saint Isidore Guild'
     }
   }
